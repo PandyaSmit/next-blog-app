a9fad4381b31e6157b1bb72fe079508b762a3eec
Smit Pandya <smitpandya20@gmail.com>
2022-09-24 13:25:59 +0530

    Fixed lint and added export

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -56,7 +56,7 @@ const Home: NextPage = () => {
                           </button>
                         </div>
                         <div className='flex items-center justify-between pb-6'>
-                          <p className='mb-0 mr-2'>Don't have an account?</p>
+                          <p className='mb-0 mr-2'>Don`&apos;`t have an account?</p>
                           <button
                             type='button'
                             className='inline-block px-6 py-2 border-2 border-green-600 text-green-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out'
