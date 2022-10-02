34b531aa5a63f229055f61bd393327aaa1b32b87
Smit Pandya <smitpandya20@gmail.com>
2022-10-02 18:47:22 +0530

    fixed url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const MONGO_DATABASE = {
-    URL: process.env.MONGO_DB_URL || 'mongodb+srv://smitzroot:trws0TNOL7CrkM2L@cluster0.grl44.mongodb.net/blog-app'
+    URL: process.env.MONGO_DB_URL
 }
